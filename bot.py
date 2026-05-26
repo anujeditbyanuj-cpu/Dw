2ce9bdc04edb2f59d224881c2124bf32178cf0de
anujeditbyanuj-cpu <anujeditbyanuj@gmail.com>
2026-05-26 12:43:28 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -77,7 +77,7 @@ logger = logging.getLogger("DiskWala-Bot")
 # ─────────────────────────────────────────
 # Config
 # ─────────────────────────────────────────
-BOT_TOKEN     = os.environ.get("BOT_TOKEN", "")
+BOT_TOKEN     = os.environ.get("BOT_TOKEN", "8883806794:AAEKZ9fUFRBJujDlHCDUp8HRQQkkLUTex6c")
 BOT_USERNAME  = os.environ.get("BOT_USERNAME", "save_restricted_ak_content_bot")
 WEBHOOK_URL   = os.environ.get("WEBHOOK_URL", "")
 PORT          = int(os.environ.get("PORT", 5000))
@@ -111,11 +111,11 @@ UPI_NAME = os.environ.get("UPI_NAME", "ANUJ")
 DISKWALA_API_KEY  = os.environ.get("DISKWALA_API_KEY", "69ffffb2d96f638117a71ea4")
 DISKWALA_BASE_URL = os.environ.get("DISKWALA_BASE_URL", "https://ddudapidd.diskwala.com/api/v1")
 
-MONGO_DB_URI = os.environ.get("MONGO_DB_URI", "")
+MONGO_DB_URI = os.environ.get("MONGO_DB_URI", "mongodb+srv://Anujedit:Anujedit@cluster0.7cs2nhd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 DB_CHANNEL   = int(os.environ.get("DB_CHANNEL", "-1003873749415"))
 
 API_ID   = int(os.environ.get("API_ID", 20432885))
-API_HASH = os.environ.get("API_HASH", "")
+API_HASH = os.environ.get("API_HASH", "4fdcfab1c7f5e24ae69f3ce6bb234dec")
 
 _support_tokens_env = os.environ.get("SUPPORT_BOT_TOKENS", "")
 SUPPORT_BOT_TOKENS = [t.strip() for t in _support_tokens_env.split(",") if t.strip()]
